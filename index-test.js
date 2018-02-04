928be384cba2800a74c49ad85f351b8998a84a98
Aria Stewart <aredridel@dinhe.net>
2018-02-03 22:25:37 -0500

    Basic tests

--- index-test.js
+++ index-test.js
@@ -1,7 +1,40 @@
 const jldq = require('./')
 const lg = require('levelgraph')
-const test = require('estap').createSuite()
+const TestRunner = require('test-runner')
+const runner = new TestRunner()
 const memdb = require('memdb')
 const db = lg(memdb())
+const assert = require('assert')
 
-test('query', t => jldq(db, {}, {}))
+runner.test('no arguments is rejected', async () => {
+  try {
+    await jldq()
+  } catch (e) {
+    assert.equal(e.message, 'Expected 2 or 3 argument but got 0')
+  }
+})
+
+runner.test('only one argument is rejected', async () => {
+  try {
+    await jldq(db)
+  } catch (e) {
+    assert.equal(e.message, 'Expected 2 or 3 argument but got 1')
+  }
+})
+
+runner.test('empty query is rejected', async () => {
+  try {
+    await jldq(db, {})
+  } catch (e) {
+    assert.equal(e.message, 'Invalid JSON-LD syntax; a JSON-LD frame must be a single object.')
+  }
+})
+
+runner.test('Simple query is accepted', async () => {
+    await jldq(db, {
+      '@context': {
+        'test': 'https://example.org/'
+      }
+    })
+})
+
