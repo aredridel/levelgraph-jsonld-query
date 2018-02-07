1dcd001ab0080c6bf7c3c71297d4014bea801a71
Aria Stewart <aredridel@dinhe.net>
2018-02-07 00:01:03 -0500

    MAke that simple test pass

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jsonld = require('jsonld').promises;
+const jsonldraw = require('jsonld')
 const aproba = require('aproba')
 
 /**
@@ -92,13 +93,13 @@ module.exports = async function query(db, frame, options) {
   opts.skipExpansion = true;
   opts.link = {};
 
-  const compacted = await jsonld.compact(framed, ctx, opts)
+  const { compacted, ctx: activeCtx } = await jsonldcompact(framed, ctx, opts)
 
   // get graph alias
-  const graph = _compactIri(ctx, '@graph');
+  const graph = _compactIri(activeCtx, '@graph');
   // remove @preserve from results
   opts.link = {};
-  compacted[graph] = _removePreserve(ctx, compacted[graph], opts);
+  compacted[graph] = _removePreserve(activeCtx, compacted[graph], opts);
 
   return compacted;
 };
@@ -880,3 +881,147 @@ function _selectTerm(activeCtx, iri, value, containers, typeOrLanguage, typeOrLa
 
   return null;
 }
+
+/**
+ * Gets the initial context.
+ *
+ * @param options the options to use:
+ *          [base] the document base IRI.
+ *
+ * @return the initial context.
+ */
+function _getInitialContext(options) {
+  var base = jsonld.url.parse(options.base || '');
+  return {
+    '@base': base,
+    mappings: {},
+    inverse: null,
+    getInverse: _createInverseContext,
+    clone: _cloneActiveContext
+  };
+
+  /**
+   * Generates an inverse context for use in the compaction algorithm, if
+   * not already generated for the given active context.
+   *
+   * @return the inverse context.
+   */
+  function _createInverseContext() {
+    var activeCtx = this;
+
+    // lazily create inverse
+    if(activeCtx.inverse) {
+      return activeCtx.inverse;
+    }
+    var inverse = activeCtx.inverse = {};
+
+    // handle default language
+    var defaultLanguage = activeCtx['@language'] || '@none';
+
+    // create term selections for each mapping in the context, ordered by
+    // shortest and then lexicographically least
+    var mappings = activeCtx.mappings;
+    var terms = Object.keys(mappings).sort(_compareShortestLeast);
+    for(var i = 0; i < terms.length; ++i) {
+      var term = terms[i];
+      var mapping = mappings[term];
+      if(mapping === null) {
+        continue;
+      }
+
+      var container = mapping['@container'] || '@none';
+
+      // iterate over every IRI in the mapping
+      var ids = mapping['@id'];
+      if(!Array.isArray(ids)) {
+        ids = [ids];
+      }
+      for(var ii = 0; ii < ids.length; ++ii) {
+        var iri = ids[ii];
+        var entry = inverse[iri];
+
+        // initialize entry
+        if(!entry) {
+          inverse[iri] = entry = {};
+        }
+
+        // add new entry
+        if(!entry[container]) {
+          entry[container] = {
+            '@language': {},
+            '@type': {}
+          };
+        }
+        entry = entry[container];
+
+        if(mapping.reverse) {
+          // term is preferred for values using @reverse
+          _addPreferredTerm(mapping, term, entry['@type'], '@reverse');
+        } else if('@type' in mapping) {
+          // term is preferred for values using specific type
+          _addPreferredTerm(mapping, term, entry['@type'], mapping['@type']);
+        } else if('@language' in mapping) {
+          // term is preferred for values using specific language
+          var language = mapping['@language'] || '@null';
+          _addPreferredTerm(mapping, term, entry['@language'], language);
+        } else {
+          // term is preferred for values w/default language or no type and
+          // no language
+          // add an entry for the default language
+          _addPreferredTerm(mapping, term, entry['@language'], defaultLanguage);
+
+          // add entries for no type and no language
+          _addPreferredTerm(mapping, term, entry['@type'], '@none');
+          _addPreferredTerm(mapping, term, entry['@language'], '@none');
+        }
+      }
+    }
+
+    return inverse;
+  }
+
+  /**
+   * Adds the term for the given entry if not already added.
+   *
+   * @param mapping the term mapping.
+   * @param term the term to add.
+   * @param entry the inverse context typeOrLanguage entry to add to.
+   * @param typeOrLanguageValue the key in the entry to add to.
+   */
+  function _addPreferredTerm(mapping, term, entry, typeOrLanguageValue) {
+    if(!(typeOrLanguageValue in entry)) {
+      entry[typeOrLanguageValue] = term;
+    }
+  }
+
+  /**
+   * Clones an active context, creating a child active context.
+   *
+   * @return a clone (child) of the active context.
+   */
+  function _cloneActiveContext() {
+    var child = {};
+    child['@base'] = this['@base'];
+    child.mappings = _clone(this.mappings);
+    child.clone = this.clone;
+    child.inverse = null;
+    child.getInverse = this.getInverse;
+    if('@language' in this) {
+      child['@language'] = this['@language'];
+    }
+    if('@vocab' in this) {
+      child['@vocab'] = this['@vocab'];
+    }
+    return child;
+  }
+}
+
+function jsonldcompact(input, ctx) {
+  return new Promise((accept, reject) => {
+    jsonldraw.compact(input, ctx, (err, compacted, ctx) => {
+      if (err) return reject(err)
+      return accept({ ctx, compacted })
+    })
+  })
+}
+
